a175d94a632175dfab6e568f7fbf19acbf99d018
Ton Duy Truong 2280603502 <duytruongk22@gmail.com>
2026-03-31 10:11:59 +0700

    init project

---
+++ src/main/java/com/example/aurabeauty/AurabeautyApplication.java
@@ -0,0 +1,13 @@
+package com.example.aurabeauty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AurabeautyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AurabeautyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aurabeauty/AurabeautyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aurabeauty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AurabeautyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
